2122f128e64debfa9ba9d895ffed5958da4585e0
AlanCheen <2985697630@qq.com>
2015-07-21 09:43:48 +0800

    add OrientationEventListener

--- PracticeDemos/app/src/main/java/yifeiyuan/practice/practicedemos/screenorientation/OrientationActivity.java
+++ PracticeDemos/app/src/main/java/yifeiyuan/practice/practicedemos/screenorientation/OrientationActivity.java
@@ -1,16 +1,24 @@
 package yifeiyuan.practice.practicedemos.screenorientation;
 
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.OrientationEventListener;
+import android.view.Surface;
 
 import yifeiyuan.practice.practicedemos.R;
 import yifeiyuan.practice.practicedemos.base.BaseActivity;
 
+/**
+ * 参考 : http://ticktick.blog.51cto.com/823160/1301209
+ */
 public class OrientationActivity extends BaseActivity {
+
+    private MyOrientationEventListener myOrientationEventListener;
 
     @Override
     protected int getLayoutId() {
@@ -28,6 +36,8 @@ public class OrientationActivity extends BaseActivity {
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         Log.d(TAG, "onConfigurationChanged() called with " + "newConfig = [" + newConfig + "]");
+        //Notice 必须调用super!! 否则会崩溃 android.util.SuperNotCalledException: Activity screenorientation.OrientationActivity did not call through to super.onConfigurationChanged()
+        super.onConfigurationChanged(newConfig);
 
 //        newConfig.getLayoutDirection();//RTL or LTR
 
@@ -36,8 +46,24 @@ public class OrientationActivity extends BaseActivity {
         } else if (Configuration.ORIENTATION_PORTRAIT == newConfig.orientation) {
             Log.d(TAG, "onConfigurationChanged :" + "portrait 竖屏");
         }
-        super.onConfigurationChanged(newConfig);//必须调用super!! 否则会崩溃 android.util.SuperNotCalledException: Activity screenorientation.OrientationActivity did not call through to super.onConfigurationChanged()
 
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                Log.d(TAG, "onConfigurationChanged rotation:" +"0");
+                break;
+            case Surface.ROTATION_90:
+                Log.d(TAG, "onConfigurationChanged rotation:" +"90");
+                break;
+            case Surface.ROTATION_180:
+                Log.d(TAG, "onConfigurationChanged rotation:" +"180");
+                break;
+            case Surface.ROTATION_270:
+                Log.d(TAG, "onConfigurationChanged rotation:" +"270");
+                break;
+        }
+        Log.d(TAG, "onConfigurationChanged rotation:" + rotation);
     }
 
     @Override
@@ -49,6 +75,10 @@ public class OrientationActivity extends BaseActivity {
 //        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);// 横屏
 //        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);// 竖屏
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);// 传感器
+
+
+        myOrientationEventListener = new MyOrientationEventListener(this);
+        myOrientationEventListener.enable();
 
     }
 
@@ -105,5 +135,26 @@ public class OrientationActivity extends BaseActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy ");
+
+        //Notice 必须disable 不然页面关闭后  它依然在工作 不断打印
+        myOrientationEventListener.disable();
     }
+
+
+    private class MyOrientationEventListener extends OrientationEventListener {
+
+        public MyOrientationEventListener(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void onOrientationChanged(int orientation) {
+            Log.d(TAG, "onOrientationChanged() called with " + "orientation = [" + orientation + "]");
+
+            //Notice  手机水平放置的时候是-1  OrientationEventListener.ORIENTATION_UNKNOWN
+            //手机竖直是0 按顺时针方向 0-359
+
+        }
+    }
+
 }
